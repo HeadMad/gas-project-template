b8b8b6a618d46defb5c72baac1fc303daff4f0ea
HeadMad <kov.ps@ya.ru>
2021-11-29 02:37:12 +0800

    add deploing command

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Initialize clasp. You can create GAS repository with commands:
 - ```npm run init-clasp``` create repo with name of npm package name
 - ```npm run init-clasp title:YOUR_TITLE``` with your custom name
 - ```npm run init-clasp clone:SCRIPT_ID``` cloning created cript
+
+## Deploing project
+- ```npm run deploy``` automaticli pushing and deploing project
--- util.js
+++ util.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
-const path = require('path');
 const { execSync } = require('child_process');
-const claspConfig = require('./.clasp.json');
-const { basename } = require('path');
+const packageConfig = require('./package.json');
 
 const actions = {};
 const action = process.argv[2];
@@ -18,19 +16,12 @@ function run(func) {
   return result
 }
 
-
-actions.init = async () => {
-  let result = await run('clasp create --type webapp --rootDir ./appsscript');
-  fs.rename('./appsscript/.clasp.json', './.clasp.json',  err => err ?? console.log);
-  let data = fs.readFileSync('./.clasp.json', 'utf8');
-  console.log(result)
-}
-
 actions.deploy = () => {
-  let result = run('clasp deploy');
-  let devID = result.match(/-\s([^\s]+)\s/)[1];
-  claspConfig.deploymentId = devID; 
-  fs.writeFileSync('.clasp.json', JSON.stringify(claspConfig));
+  let result = run('clasp push -f && clasp deploy');
+  let deploymentId = result.match(/-\s([^\s]+)\s/)[1];
+  // "deploy": "node util.js deploy"
+  packageConfig.scripts.deploy = 'clasp push && clasp deploy --deploymentId ' + deploymentId;
+  fs.writeFileSync('package.json', JSON.stringify(packageConfig, null, '  '));
   console.log(result);
 }
 
